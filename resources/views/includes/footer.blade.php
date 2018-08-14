6c155454cd36d93f1353349f817471b977f23ef7
davoliva <dav.bahamondez@gmail.com>
2018-08-14 00:12:02 -0300

    Se agrego una sub-vista footer

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,35 @@
+<footer class="footer footer-default">
+    <div class="container">
+        <nav class="float-left">
+        <ul>
+            <li>
+            <a href="#">
+                ¿Necesitas ayuda con una proyecto?
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                Asesoría personalizada
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                Blog
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                Tutoriales sobre desarrollo web
+            </a>
+            </li>
+        </ul>
+        </nav>
+        <div class="copyright float-right">
+            &copy;
+            <script>
+                document.write(new Date().getFullYear())
+            </script>, made with <i class="material-icons">favorite</i> by
+            <a href="#" target="_blank">Kotaix</a>.
+        </div>
+    </div>
+</footer>
